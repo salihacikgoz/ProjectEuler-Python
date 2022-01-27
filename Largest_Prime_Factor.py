af5fcdba72f39aa6ed917961d514f6bcd1f6f37c
salihacikgoz <salihacikgoz_business@outlook.com>
2022-01-28 01:45:31 +0300

    Euler Projesi - Python

--- Largest_Prime_Factor.py
+++ Largest_Prime_Factor.py
@@ -1,7 +1,7 @@
 sayac = 0
 
 for i in range(1,600851475143):
-    if(13195 % i == 0):
+    if(600851475143 % i == 0):
             for j in range(1,i + 1):
                 if(i % j == 0):
                     sayac = sayac + 1
